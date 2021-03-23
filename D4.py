68d114bcf5c4341c0fd98e73242adfbe97c55665
Aryano Devil <81142233+aryanotriks@users.noreply.github.com>
2021-03-22 23:15:21 -0700

    Create D4.py

---
+++ D4.py
@@ -0,0 +1,227 @@
+import os
+import time
+import sys
+os.system ('clear')
+time.sleep (1)
+print ("\033[32;1m                                     ___")
+time.sleep(0.07)
+print ("\033[32;1m================================____|  /_")
+time.sleep(0.07)
+print "\033[34;1m|"+" \033[33;1mTools :"+" Install Tools D4          \033[32;1m|_____"
+time.sleep(0.07)
+print "\033[34;1m|"+" \033[33;1mAuthor:"+" 4RY4N0-D3V1L                          \033[32;1m|"
+time.sleep(0.07)
+print "\033[34;1m|"+" \033[33;1mThx To:"+" Termux Tools ID                      \033[32;1m|_"
+time.sleep(0.07)
+print "\033[34;1m|"+" \033[33;1mThx To: "+"4RY4N0 (S4YY3D Z4k4RY4)" + "\033[32;1m         ___|"
+time.sleep(0.07)
+print "\033[32;1m================================_____________|   |"
+time.sleep(0.07)
+print("\033[32;1m[1 ]\033[1;37mTUANB4DUTv1  \033[33;1m(\033[37;1m46  \033[31;1mTools\033[33;1m)                     \033[32;1m|")
+time.sleep(0.07)
+print("\033[32;1m[2 ]\033[1;37mTUANB4DUTv2  \033[33;1m(\033[37;1m101 \033[31;1mTools\033[33;1m)\033[32;1m                   __|")
+time.sleep(0.07)
+print("\033[32;1m[4 ]\033[1;37mTUANB4DUTv3  \033[33;1m(\033[37;1m311 \033[31;1mTools\033[33;1m)                  \033[32;1m|")
+time.sleep(0.07)
+print("\033[32;1m[4 ]\033[1;37mTUANB4DUTv4  \033[33;1m(\033[37;1m401 \033[31;1mTools\033[33;1m)                 \033[32;1m |")
+time.sleep(0.07)
+print("\033[32;1m[5 ]\033[1;37mTU4NN1CKN4M3 \033[33;1m(\033[37;1m52  \033[31;1mTools\033[33;1m)                  \033[32;1m|")
+time.sleep(0.07)
+print("\033[32;1m[6 ]\033[1;37mB4J1N64Nv1   \033[33;1m(\033[37;1m29  \033[31;1mTools\033[33;1m)   \033[32;1m [0]\033[1;37mExit       \033[32;1m|")
+time.sleep(0.07)
+print("\033[32;1m[7 ]\033[1;37mB4J1N64Nv2   \033[33;1m(\033[37;1m41  \033[31;1mTools\033[33;1m)                  \033[32;1m|")
+time.sleep(0.07)
+print("\033[32;1m[8 ]\033[1;37mB4J1N64Nv3   \033[33;1m(\033[37;1m51  \033[31;1mTools\033[33;1m)                  \033[32;1m|")
+time.sleep(0.07)
+print("\033[32;1m[9 ]\033[1;37mB4J1N64Nv4   \033[33;1m(\033[37;1m80  \033[31;1mTools\033[33;1m)                  \033[32;1m|")
+time.sleep(0.07)
+print("\033[32;1m[10]\033[1;37mB4J1N64Nv5   \033[33;1m(\033[37;1m100 \033[31;1mTools\033[33;1m)                  \033[32;1m|")
+time.sleep(0.07)
+print("\033[32;1m[11]\033[1;37mB4J1N64Nv6   \033[33;1m(\033[37;1m200 \033[31;1mTools\033[33;1m)                  \033[32;1m|")
+time.sleep(0.07)
+print("\033[32;1m[12]\033[1;37mBYE          \033[33;1m(\033[37;1m25  \033[31;1mTools\033[33;1m)                  \033[32;1m|")
+time.sleep(0.07)
+print("\033[32;1m[13]\033[1;37mDarkFly      \033[33;1m(\033[37;1m530 \033[31;1mTools\033[33;1m) \033[32;1m_________________|")
+time.sleep(0.07)
+print ("\033[32;1m             _______________|")
+time.sleep(0.06)
+print ("\033[32;1m         ___|")
+time.sleep(0.06)
+print ("\033[32;1m        /")
+time.sleep(0.06)
+print ("\033[32;1m       /")
+time.sleep(0.06)
+try:
+  pilih = input("      ")
+except NameError:
+  print ("\033[1;37mPilihnya Pake Angka Bossq :*")
+  sys.exit()
+except SyntaxError:
+  print ("\033[32;1mAnying Terkacangi T_T")
+  sys.exit()
+if pilih ==1:
+  try:
+    os.chdir ("TOOLSINSTALLERv1")
+    os.system ("sh Tuanb4dut.sh")
+  except:
+    os.system ('figlet "Zen"|lolcat')
+    os.system ("apt update && apt upgrade")
+    os.system ("pip2 install lolcat")
+    os.system ("pkg install figlet")
+    os.system ("pkg install php")
+    os.system ("git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1")
+    os.chdir ("TOOLSINSTALLERv1")
+    os.system ("chmod +x Tuanb4dut.sh")
+    os.system ("sh Tuanb4dut.sh")
+if pilih ==2:
+  try:
+    os.chdir ("TOOLSINSTALLERv2")
+    os.system ("sh Tuanb4dut.sh")
+  except:
+    os.system ('figlet "Zen"|lolcat')
+    os.system ("apt update && apt upgrade")
+    os.system ("pip2 install lolcat")
+    os.system ("pkg install figlet")
+    os.system ("pkg install php")
+    os.system ("git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv2")
+    os.chdir ("TOOLSINSTALLERv2")
+    os.system ("chmod +x Tuanb4dut.sh")
+    os.system ("sh Tuanb4dut.sh")
+if pilih ==3:
+  try:
+    os.chdir ("TOOLSINSTALLERv3")
+    os.system ("sh TUANB4DUT.sh")
+  except:
+    os.system ('figlet "Zen"|lolcat')
+    os.system ("apt update && apt upgrade")
+    os.system ("pkg install python python2 vim figlet curl")
+    os.system ("pkg install php")
+    os.system ("pip2 install lolcat")
+    os.system ("git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3")
+    os.chdir ("TOOLSINSTALLERv3")
+    os.system ("chmod +x TUANB4DUT.sh")
+    os.system ("sh TUANB4DUT.sh")
+if pilih ==4:
+  try:
+    os.chdir ("TOOLSINSTALLERv4")
+    os.system ("sh TUANB4DUT.sh")
+  except:
+    os.system ('figlet "Zen"|lolcat')
+    os.system ("apt update && apt upgrade")
+    os.system ("pkg install python python2 vim figlet curl")
+    os.system ("pkg install php")
+    os.system ("pip2 install lolcat")
+    os.system ("git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4")
+    os.chdir ("TOOLSINSTALLERv4")
+    os.system ("chmod 777 TUANB4DUT.sh")
+    os.system ("sh TUANB4DUT.sh")
+if pilih ==5:
+  try:
+    os.chdir ("TOOLSv2")
+    os.system ("sh TU4NN1CKN4M3.sh")
+  except:
+    os.system ('figlet "Zen"|lolcat')
+    os.system ("apt update && apt upgrade")
+    os.system ("pkg install python python2 vim figlet curl")
+    os.system ("pip2 install lolcat")
+    os.system ("pkg install git")
+    os.system ("git clone https://github.com/TU4NN1CKN4M3/TOOLSv2")
+    os.chdir ("TOOLSv2")
+    os.system ("sh TU4NN1CKN4M3.sh")
+if pilih ==6:
+  try:
+    os.chdir ("T00Ls-B4J1N64N")
+    os.system ("./B4J1N64N")
+  except:
+    os.system ('figlet "Zen"|lolcat')
+    os.system ("apt update && apt upgrade")
+    os.system ("pkg install figlet toilet")
+    os.system ("git clone https://github.com/DarknessCyberTeam/T00Ls-B4J1N64N")
+    os.chdir ("T00Ls-B4J1N64N")
+    os.system ("chmod 777 B4J1N64N")
+    os.system ("./B4J1N64N")
+if pilih ==7:
+  try:
+    os.chdir ("T00Ls-B4J1N64N2")
+    os.system ("sh B4J1N64N.sh")
+  except:
+    os.system ('figlet "Zen"|lolcat')
+    os.system ("apt update && apt upgrade")
+    os.system ("pkg install figlet toilet")
+    os.system ("git clone https://github.com/DarknessCyberTeam/T00Ls-B4J1N64N2")
+    os.chdir ("T00Ls-B4J1N64N2")
+    os.system ("chmod 777 B4J1N64N.sh")
+    os.system ("sh B4J1N64N.sh")
+if pilih ==8:
+  try:
+    os.chdir ("T00Ls-B4J1N64Nv3")
+    os.system ("sh B4J1N64N.sh")
+  except:
+    os.system ('figlet "Zen"|lolcat')
+    os.system ("apt update && apt upgrade")
+    os.system ("pkg install figlet toilet")
+    os.system ("git clone https://github.com/DarknessCyberTeam/T00Ls-B4J1N64Nv3")
+    os.chdir ("T00Ls-B4J1N64Nv3")
+    os.system ("chmod 777 B4J1N64N.sh")
+    os.system ("sh B4J1N64N.sh")
+if pilih ==9:
+  try:
+    os.chdir ("B4J1N64Nv4")
+    os.system ("sh bajingan.sh")
+  except:
+    os.system ('figlet "Zen"|lolcat')
+    os.system ("apt update && apt upgrade")
+    os.system ("pkg install figlet toilet")
+    os.system ("git clone https://github.com/DarknessCyberTeam/B4J1N64Nv4")
+    os.chdir ("B4J1N64Nv4")
+    os.system ("chmod 777 bajingan.sh")
+    os.system ("sh bajingan.sh")
+if pilih ==10:
+  try:
+    os.chdir ("B4J1N64Nv5")
+    os.system ("sh B4J1N64N.sh")
+  except:
+    os.system ('figlet "Zen"|lolcat')
+    os.system ("apt update && apt upgrade")
+    os.system ("pkg install figlet toilet")
+    os.system ("git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5")
+    os.chdir ("B4J1N64Nv5")
+    os.system ("chmod 777 B4J1N64N.sh")
+    os.system ("sh B4J1N64N.sh")
+if pilih ==11:
+  try:
+    os.chdir ("BAJINGANv6")
+    os.system ("sh BAJINGAN.sh")
+  except:
+    os.system ('figlet "Zen"|lolcat')
+    os.system ("apt update && apt upgrade")
+    os.system ("pip2 install termcolor")
+    os.system ("pip2 install lolcat")
+    os.system ("pkg install toilet")
+    os.system ("git clone https://github.com/DarknessCyberTeam/BAJINGANv6")
+    os.chdir ("BAJINGANv6")
+    os.system ("sh BAJINGAN.sh")
+if pilih ==12:
+    os.system ("python2 BYE.py")
+if pilih ==13:
+  try:
+    os.chdir ("DarkFly-Tool")
+    os.system ("DarkFly")
+  except:
+    os.system ('figlet "Zen"|lolcat')
+    os.system ("pkg install python2")
+    os.system ("pkg install php")
+    os.system ("git clone https://github.com/Ranginang67/DarkFly-Tool")
+    os.chdir ("DarkFly-Tool")
+    os.system ("chmod +x install.py")
+    os.system ("python2 install.py")
+    os.system ("DarkFly")
+if pilih ==0:
+    print ("No Caption!  >:(")
+    time.sleep(1)
+    print ("Lelah Gw :v !")
+    time.sleep(1)
+    sys.exit()
+else:
+    print ("\033[31;1m[!] Keluar")
+    sys.exit()
